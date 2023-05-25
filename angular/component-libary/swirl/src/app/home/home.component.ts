5af9014ba47fc8ee2c110be1bd735758fb20a83c
Christian Richter <Christian.Richter@empulse.de>
2023-05-25 15:28:49 +0200

    added button and text

--- angular/component-libary/swirl/src/app/home/home.component.ts
+++ angular/component-libary/swirl/src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild('myInput', { static: true })
+  myInput!: ElementRef;
+
+  inputText: string = 'Message from component';
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.myInput.nativeElement.value = '';
+  }
 
 }
